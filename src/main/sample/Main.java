1860dd23db0f10e6bb4e812590418a6608d12275
katorek <wojtasj1996@gmail.com>
2016-10-07 15:44:24 +0200

    Images works from inside now

--- src/main/sample/Main.java
+++ src/main/sample/Main.java
@@ -3,26 +3,23 @@ package main.sample;
 import games.Tabs;
 import javafx.application.Application;
 import javafx.application.Platform;
-import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.Label;
 import javafx.scene.control.TabPane;
 import javafx.scene.control.TextField;
-import javafx.scene.image.Image;
 import javafx.stage.Stage;
-import javafx.stage.WindowEvent;
 
 import javax.imageio.ImageIO;
 import java.awt.*;
-import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.File;
 import java.io.IOException;
 
 public class Main extends Application {
 
+    private static TabPane tabPane;
     private Button monitorButton, simulateClick;
     private TextField xCord, yCord;
     private CheckBox mouseFirstLocationCheckBox;
@@ -30,45 +27,55 @@ public class Main extends Application {
     private Robot bot;
     private Label runningTime, mouseFirstLocationLabel, xLabel, yLabel;
     private int cycle;
-    private static TabPane tabPane;
     private TrayIcon trayIcon;
     private boolean firstTime;
     private java.awt.Image imageIcon;
-    private void stopMonitor() {
-        started = false;
-        monitorButton.setText("Start Monitor");
-    }
-    private void startMonitor() {
-        started = true;
-        monitorButton.setText("Stop Monitor !");
-    }
 
-
-    public static void disableTabs(int i, boolean status){
-        switch (i){
+    public static void disableTabs(int i, boolean status) {
+        switch (i) {
             case 0: {
 //                DotaTab
             }
         }
     }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    private void stopMonitor() {
+        started = false;
+        monitorButton.setText("Start Monitor");
+    }
+
+    private void startMonitor() {
+        started = true;
+        monitorButton.setText("Stop Monitor !");
+    }
+
     @Override
     public void start(Stage stage) throws Exception {
-//        Parent root = FXMLLoader.load(getClass().getResource("main.sample.fxml"));
-        createTrayIcon(stage);
-        tabPane = new Tabs().getTabPane();
-        Scene scene = new Scene(tabPane, 300, 250);
+        createIcons(stage);
+//        tabPane = new Tabs().getTabPane();
+        Scene scene = new Scene(new Tabs().getTabPane(), 300, 250);
         firstTime = true;
         Platform.setImplicitExit(false);
         scene.getStylesheets().add(Main.class.getResource("style.css").toExternalForm());
-        imageIcon = ImageIO.read(new File("src/img/icon.png"));
-        stage.getIcons().add(new Image( new File("src/img/icon2.png").toURI().toString()));
+//        imageIcon = ImageIO.read(new File("src/img/icon.png"));
         stage.setScene(scene);
         stage.setResizable(false);
         stage.setTitle("Auto Accepter for games");
         stage.show();
     }
 
-    public void createTrayIcon(final Stage stage) {
+    private void createIcons(final Stage stage) {
+//        ClassLoader cl = this.getClass().getClassLoader(); //test for learning
+//        Icon icon = new ImageIcon(cl.getResource("img/icon.png"));
+        stage.getIcons().add(new javafx.scene.image.Image(Main.class.getResourceAsStream("/img/icon.png")));
+        createTrayIcon(stage);
+    }
+
+    private void createTrayIcon(final Stage stage) {
         if(SystemTray.isSupported()){
             SystemTray tray = SystemTray.getSystemTray();
             java.awt.Image image = null;
@@ -78,31 +85,11 @@ public class Main extends Application {
                 e.printStackTrace();
             }
 
-            stage.setOnCloseRequest(new EventHandler<WindowEvent>() {
-                @Override
-                public void handle(WindowEvent event) {
-                    hide(stage);
-                }
-            });
+            stage.setOnCloseRequest(event -> hide(stage));
 
-            final ActionListener closeListener = new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    System.exit(0);
-                }
-            };
+            final ActionListener closeListener = e -> System.exit(0);
 
-            ActionListener showListener = new ActionListener() {
-                @Override
-                public void actionPerformed(ActionEvent e) {
-                    Platform.runLater(new Runnable() {
-                        @Override
-                        public void run() {
-                            stage.show();
-                        }
-                    });
-                }
-            };
+            ActionListener showListener = e -> Platform.runLater(stage::show);
 
             PopupMenu popup = new PopupMenu();
 
@@ -110,15 +97,18 @@ public class Main extends Application {
             showItem.addActionListener(showListener);
             popup.add(showItem);
 
-            MenuItem dotaAccept = new MenuItem("Dota AutoAccept");
-            MenuItem csgoAccept = new MenuItem("Csgo AutoAccept");
-            MenuItem paladinsAccept = new MenuItem("Paladins AutoAccept");
+//            In plans: execute auto start click from tray menu
+//            MenuItem dotaAccept = new MenuItem("Dota AutoAccept");
+//            MenuItem csgoAccept = new MenuItem("Csgo AutoAccept");
+//            MenuItem paladinsAccept = new MenuItem("Paladins AutoAccept");
 
             MenuItem closeItem = new MenuItem("Close");
             closeItem.addActionListener(closeListener);
             popup.add(closeItem);
 
-            trayIcon = new TrayIcon(image, "AutoAccept",popup);
+            if (image != null) {
+                trayIcon = new TrayIcon(image, "AutoAccept", popup);
+            }
             trayIcon.addActionListener(showListener);
             try{
                 tray.add(trayIcon);
@@ -128,32 +118,23 @@ public class Main extends Application {
         }
     }
 
-    public void showProgramIsMinimizedMsg() {
+    private void hide(final Stage stage) {
+        Platform.runLater(() -> {
+            if (SystemTray.isSupported()) {
+                stage.hide();
+                showProgramIsMinimizedMsg();
+            } else
+                System.exit(0);
+
+        });
+    }
+
+    private void showProgramIsMinimizedMsg() {
         if (firstTime) {
             trayIcon.displayMessage("Minimized",
                     "Minimized to tray",
                     TrayIcon.MessageType.INFO);
             firstTime = false;
         }
-    }
-
-    private void hide(final Stage stage) {
-        Platform.runLater(new Runnable() {
-            @Override
-            public void run() {
-                if(SystemTray.isSupported()) {
-                    stage.hide();
-                    showProgramIsMinimizedMsg();
-                }
-                else
-                    System.exit(0);
-
-            }
-        });
-    }
-
-
-    public static void main(String[] args) {
-        launch(args);
     }
 }
